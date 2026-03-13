6959baf7bc23a5253b0457ef10faf66e401a686d
Chideraakude <akudechidera@gmail.com>
2026-03-13 10:43:56 +0100

    AirConditioning

---
+++ 2026/AirConditioner/ArrayOfNumbers.java
@@ -0,0 +1,37 @@
+
+
+
+public class ArrayOfNumbers{
+
+    public static void main(String[]args){
+
+    int [] numbers = {2, 5, 7, 1, 6, 9}
+    evenOrOdd(numbers);
+    
+
+
+
+
+    }
+
+
+    public static void evenOrOdd(int [] numbers){
+        for(int count = 0; count <= numbers.length; count++){
+
+            if(int numbers [count] % 2 == 0){
+                System.out.println(true);
+        }
+
+            }
+        else{
+            System.out.println(false);
+        }
+
+
+
+    }
+
+
+
+
+}
